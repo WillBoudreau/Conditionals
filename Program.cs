f0ace5171ae4ccfcbb97cb6b398744be505a1cd5
WillBoudreau <williamboudreau804@gmail.com>
2023-09-25 11:57:46 -0300

    Update Sep 25 2023

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Conditionals
@@ -15,6 +16,7 @@ namespace Conditionals
         static int playerdamage;
         static int lives;
         static string WeaponSelection;
+        static string Start;
 
         static void Main(string[] args)
         {
@@ -25,6 +27,12 @@ namespace Conditionals
             health = 100;
             monsterdamage = 10;
             lives = 3;
+            Console.WriteLine("Are you ready to begin? " + "Type Yes to begin");
+            Start = Console.ReadLine();
+            if(Start == "Yes")
+            {
+                ShowHUD();
+            }
             TakeDamage();
         }
         
@@ -33,20 +41,35 @@ namespace Conditionals
             Console.WriteLine("You are about to take");
             Console.WriteLine(monsterdamage + " Damage");
             health = health - monsterdamage;
-            monsterdamage = monsterdamage * 2;
            
             while (health > 0)
             {
                 ShowHUD();
                 TakeDamage();
-                if ( health >= 0 && health <= 10)
+                if ( health > 0 && health <= 10)
                 {
                     Console.WriteLine(health);
-                    Console.WriteLine("Health Crritcal!");
+                    Console.WriteLine("Health Critcal!");
                 }
-                if (health > 10 && health <= 20)
+                if (health > 10 && health <= 30)
                 {
-
+                    Console.WriteLine(health);
+                    Console.WriteLine("Health low!");
+                }
+                if (health > 30 && health  <= 50)
+                {
+                    Console.WriteLine(health);
+                    Console.WriteLine("Health okay");
+                }
+                if (health > 50 && health <= 75)
+                { 
+                    Console.WriteLine(health);
+                    Console.WriteLine("Health good");
+                }
+                if (health > 75 && health < 100) ;
+                {
+                    Console.WriteLine(health);
+                    Console.WriteLine("Health great");
                 }
                 if (health <= 0)
                 {
@@ -60,14 +83,16 @@ namespace Conditionals
 
             Console.ReadKey(true);
         }
-        
+        static void
+
         static void ShowHUD()
         {
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("Health " + health);
             Console.WriteLine("Choose your weapon: ");
-            Console.WriteLine("1.Pistol 2.Shotgun 3.Spreader 4.Laser 5.Sniper 6.??? or 7. to heal");
+            Console.WriteLine("1.Pistol 2.Shotgun 3.Spreader 4.Laser 5.Sniper 6.??? 7. to heal 8. Shield");
             WeaponSelection = Console.ReadLine();
-           
+            Console.WriteLine("---------------------------------");
             if (WeaponSelection == "1")
             {
                 pistol();
@@ -109,6 +134,10 @@ namespace Conditionals
             {
                 health = health + 10;
                 Console.WriteLine("Health up! +10");
+            }
+            if (WeaponSelection == "8")
+            {
+                monsterdamage = monsterdamage / 2;
             }
         }
         
